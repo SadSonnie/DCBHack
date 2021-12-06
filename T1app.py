c94e30336a71a6d2b6f55daeb3865dfd83ddaca5
broadcom9821 <44141692+broadcom9821@users.noreply.github.com>
2021-12-06 12:41:18 +0300

    Add files via upload

--- T1app.py
+++ T1app.py
@@ -1,10 +1,34 @@
 import numpy as np
-from matplotlib import pyplot as plt
-plt.rcParams["figure.figsize"] = [7.00, 3.50]
-plt.rcParams["figure.autolayout"] = True
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-data = np.random.random(size=(3, 3, 3))
-z, x, y = data.nonzero()
-ax.scatter(x, y, z, c=z, alpha=1)
-plt.show()
+garx, gary, garz = map(int, input().split())
+f1 = open(input(), 'r')
+arr  =[]
+while True:
+    # считываем строку
+    line = f1.readline()
+    # прерываем цикл, если строка пустая
+    if not line:
+        break
+    # выводим строку
+    arr.append(list(map(int, line.split())))
+# закрываем файл
+print(arr)
+f1.close()
+sidesp = (garx - 320)/2
+backsp = (gary-560)
+rowside = (sidesp - 60)/2
+sideobj = []
+backobj = []
+s = 0
+b = 0
+for i in range(0,len(arr)):
+    if arr[i][0]<=sidesp:
+        temp = arr[i].copy()
+        temp.append(i+1)
+        sideobj.append(temp)
+    if arr[i][1]<=backsp:
+        temp = arr[i].copy()
+        temp.append(i + 1)
+        backobj.append(temp)
+        b+=1
+print(sideobj)
+print(backobj)
